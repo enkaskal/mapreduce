efa5ea0ec5bf9ffde32eb0e595d9338234902909
stefan <>
2012-09-17 04:00:17 +0000

    cleaned up WordCountMR and made debugging simple.

--- WordCountMR/src/main/java/net/cryp7/range/WordCountMR/Main.java
+++ WordCountMR/src/main/java/net/cryp7/range/WordCountMR/Main.java
@@ -11,12 +11,22 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class WordCountMR 
+public class Main 
 {
-	private Job job;
-	
-	public WordCountMR(String inputPath, String outputPath) throws IOException
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException 
 	{
+		if ( 2 != args.length )
+		{
+			System.err.println("Usage: WordCountMR hdfs://server[:port]/path/to/input/dir hdfs://server[:port]/path/to/output/dir");
+			System.exit(1);
+		}
+		
 		Configuration conf = new Configuration();
 		conf.set("fs.defaultFS", "hdfs://hadoop.range.cryp7.net:8020");
 		conf.set("hbase.zookeeper.quorum", "hadoop.range.cryp7.net");
@@ -24,7 +34,7 @@ public class WordCountMR
 		conf.set("hbase.master", "hadoop.range.cryp7.net:60000");
 		conf.set("mapred.job.tracker", "hadoop.range.cryp7.net:8021");
 		
-		job = new Job(conf);
+		Job job = new Job(conf);
         job.setJobName("WordCountMR");
         job.setJarByClass(MyMapper.class);
         
@@ -37,20 +47,17 @@ public class WordCountMR
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		
-		FileInputFormat.setInputPaths(job, new Path(inputPath));
-		FileOutputFormat.setOutputPath(job, new Path(outputPath));
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
-	} /* end c-tor WordCountMR(String inputPath, String outputPath) */
-	
-	public void run() throws IOException, InterruptedException, ClassNotFoundException
-	{
 		boolean success = job.waitForCompletion(true);
 		if ( false == success )
 		{
 			System.err.println("job fuct");
 			System.exit(1);
 		}
-	}
+		
+	} /* end main */
 
 } /* end class WordCountMR */
 
